36ba6c72ca0cd846add364103bf12182fd4697e8
Rafael <rvalle@valletech.eu>
2021-10-15 15:24:59 +0200

    testing to get the imOnline event, filtering in the handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,16 +1,36 @@
 import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+
+import {EventId} from "@polkadot/types/interfaces";
+
+import {Heartbeat as SubstrateHeartbeat} from "@polkadot/types/interfaces/imOnline";
+
 import {Heartbeat} from "../types";
 
 
+const HeartBeatEvent =  "0x0c00" ;
+
 export async function handleHeartbeat(event: SubstrateEvent): Promise<void> {
 
-    const hb=new Heartbeat(
-        `${event.block.block.header.number.toNumber()}-${event.idx}`
-    );
+    const eventType:EventId = event.event.index as EventId;
 
-    hb.authorityId="TEST";
+    if (eventType.toString() === HeartBeatEvent){
 
-    await hb.save()
+        logger.info(JSON.stringify(event.extrinsic.extrinsic.toHuman()));
+
+        const eventId=`${event.block.block.header.number.toNumber()}-${event.idx}`;
+        const shb=event.extrinsic.extrinsic.method.args[0] as SubstrateHeartbeat;
+
+
+        logger.info(JSON.stringify(shb.networkState.peerId));
+
+
+        const hb=new Heartbeat(eventId);
+        hb.authorityId=event.event.data[0].toString();
+        hb.peerId=shb.networkState.peerId.toHex();
+        hb.networkAddress=shb.networkState.externalAddresses[0].toHuman().toString();
+        await hb.save()
+    }
+
 }
 
 
